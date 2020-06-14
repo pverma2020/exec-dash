bb0441461e3646fe4917b1d4a2a9f7bfbfc3b412
Gulab Jamun <65733073+pverma2020@users.noreply.github.com>
2020-06-14 15:10:15 -0400

    Add imports and utilize starter code for graph from chart gallery exercise

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,4 +1,11 @@
 # dashboard_generator.py
+import operator
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)  # > $12,000.71
 
 print("-----------------------")
 print("MONTH: March 2018")
@@ -17,3 +24,14 @@ print("  3) etc.")
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
+
+# Generate Bar Chart for data from Chart Gallery Exercise
+genre = [x["genre"] for x in bar_data]
+y_pos = np.arange(len(genre))
+views = [y["viewers"] for y in bar_data]
+plt.barh(y_pos, views, align='center')
+plt.yticks(y_pos, genre)
+plt.xlabel("Product")
+plt.title("Monthly Sales (USD)")
+plt.gcf().axes[0].xaxis.get_major_formatter().set_scientific(False)
+plt.show()
